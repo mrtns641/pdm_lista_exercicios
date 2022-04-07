a42fddd8c6f8503d7c31882135c7ef7ab0f207a0
Gabriella Martins <gabmcavalheiro641@gmail.com>
2022-04-06 22:21:29 -0300

    Resolução do exercício 3 feita

---
+++ exercicio3.js
@@ -0,0 +1,10 @@
+/*
+    3. Crie um objeto JSON que abriga todas as operações do exercício 2
+*/
+
+const operacoes = {
+    somar: (n1, n2) => n1 + n2,
+    subtrair: (n1, n2) => n1 - n2,
+    multiplicar: (n1, n2) => n1 * n2,
+    dividir: (n1, n2) => n1 / n2
+}
