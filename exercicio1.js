dbd675addfec67a98094241241c58544c9dbe7a0
Gabriella Martins <gabmcavalheiro641@gmail.com>
2022-04-06 22:17:55 -0300

    Resolução do exercício 1 feita

---
+++ exercicio1.js
@@ -0,0 +1,21 @@
+/*
+    1. Escreva funções que implementam as quatro operações aritméticas.
+    Todas elas devem receber dois valores numéricos como parâmetro e devolver 
+    o valor da sua respectiva operação. Faça a implementação usando a palavra function para todas elas.
+*/
+
+function somar (n1, n2) {
+    return n1 + n2;
+}
+
+function subtrair (n1, n2) {
+    return n1 - n2;
+}
+
+function multiplicar (n1, n2) {
+    return n1 * n2;
+}
+
+function dividir (n1, n2) {
+    return n1 / n2;
+}
