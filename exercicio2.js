6d985ad48926cb443d56e5f16e2d329a2114fcf1
Gabriella Martins <gabmcavalheiro641@gmail.com>
2022-04-06 22:19:18 -0300

    Resolução do exercício 2 feita

---
+++ exercicio2.js
@@ -0,0 +1,19 @@
+/*
+    2. Refaça o exercício 1 usando arrow functions
+*/
+
+somar = (n1, n2) => {
+    return n1 + n2;
+}
+
+subtrair = (n1, n2) => {
+    return n1 - n2;
+}
+
+multiplicar = (n1, n2) => {
+    return n1 * n2;
+}
+
+dividir = (n1, n2) => {
+    return n1 / n2;
+}
