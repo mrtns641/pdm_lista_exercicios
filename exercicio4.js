cdf5479d1ef2dbe809b9c89c725d7229bc867b24
Gabriella Martins <gabmcavalheiro641@gmail.com>
2022-04-15 21:41:04 -0300

    Arrumando o Exercicio 4

--- exercicio4.js
+++ exercicio4.js
@@ -11,8 +11,6 @@
 */
 const entrada = require('prompt-sync') ({sigint: true});
 
-var op, n1, n2;
-
 const operacoes = {
     somar: (n1, n2) => n1 + n2,
     subtrair: (n1, n2) => n1 - n2,
@@ -20,36 +18,44 @@ const operacoes = {
     dividir: (n1, n2) => n1 / n2
 }
 
-do{
-    n1 = parseInt(entrada("PRIMEIRO NÚMERO: "));
-    n2 = parseInt(entrada("SEGUNDO NÚMERO: "));
-    console.log("------------MENU------------");
-    console.log("1 - SOMA");
-    console.log("2 - SUBTRAÇÃO");
-    console.log("3 - MULTIPLICAÇÃO");
-    console.log("4 - DIVISÃO");
-    console.log("5 - SAIR");
-    console.log("----------------------------");
-    op =parseInt(entrada("Qual operação deseja realizar? "));
+ calculadora = () => {
+    var op, n1, n2;
 
-   switch (op) {
-        case 1:
-            console.log(`Soma dos números = ${operacoes.somar(n1, n2)}`);
-        break;
-        case 2:
-            console.log(`Subtração dos números = ${operacoes.subtrair(n1, n2)}`);
-        break;
-        case 3:
-            console.log(`Multiplicação dos números = ${operacoes.multiplicar(n1, n2)}`);
-        break;
-        case 4:
-            console.log(`Divisão dos números = ${operacoes.dividir(n1, n2)}`);
-        break;
-        case 5:
-            console.log("Até breve");
-        break
-        default:
-            console.log("Operação inválida, tente novamente...");
-    }
+    do{
+        n1 = parseInt(entrada("Digite o primeiro valor? "));
+        n2 = parseInt(entrada("Digite o segundo valor? "));
 
-} while (op !== 5);
+        console.log("------------MENU------------");
+        console.log("1 - SOMA");
+        console.log("2 - SUBTRAÇÃO");
+        console.log("3 - MULTIPLICAÇÃO");
+        console.log("4 - DIVISÃO");
+        console.log("5 - SAIR");
+        console.log("----------------------------");
+
+        op = parseInt(entrada("Qual operação deseja realizar? "));
+    
+       switch (op) {
+            case 1:
+                console.log(`Soma dos números = ${operacoes.somar(n1, n2)}`);
+            break;
+            case 2:
+                console.log(`Subtração dos números = ${operacoes.subtrair(n1, n2)}`);
+            break;
+            case 3:
+                console.log(`Multiplicação dos números = ${operacoes.multiplicar(n1, n2)}`);
+            break;
+            case 4:
+                console.log(`Divisão dos números = ${operacoes.dividir(n1, n2)}`);
+            break;
+            case 5:
+                console.log("Até breve");
+            break
+            default:
+                console.log("Operação inválida, tente novamente...");
+        }
+    
+    } while (op !== 5);
+}
+
+calculadora();
---
+++ exercicio5.js
@@ -0,0 +1,12 @@
+/*
+    5. Faça um programa que, utilizando promises e as funções then/catch, exibe a temperatura
+    atual (atributo chamado temp). O programa deve ser um loop que exibe as seguintes opções para o usuário:
+        1 - Digitar latitude e longitude
+        2 - Sair
+    O programa fica em loop até que o usuário digite 2. Ao digitar 1, o usuário deve informar
+    os valores de latitude da localização da qual deseja saber a temperatura atual. Use um 
+    arquivo .env e o pacote dotenv para configurar uma variável de ambiente que indica qual 
+    unidade de medida de temperatura deve ser utilizada: Kelvin, Fahrenheit ou Celsius. 
+    Lembre-se de armazenar a chave de API no arquivo .env e de tomar o cuidado de não armazená-lo 
+    no sistema de controle de versão.
+*/
